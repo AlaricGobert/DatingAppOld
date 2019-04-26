158583e5b67a97ddcb5cd95bab83848e93f8a9d7
alaric.gobert@hotmail.com <alaric.gobert@hotmail.com>
2019-04-27 01:41:48 +0200

    Before Syncfusion

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,17 +10,16 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
-      console.log(this.model);
-    }, error => {
-      console.log(this.model);
-    });
+    this.authService.login(this.model).subscribe(
+      next => this.alertify.succes(this.model),
+      error => this.alertify.error(error)
+    );
   }
 
   loggedin() {
@@ -29,7 +29,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
